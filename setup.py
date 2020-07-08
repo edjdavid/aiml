843bd20411f2e18820384c50449994cdf8886ea8
Eduardo David Jr <edjdavid@users.noreply.github.com>
2020-07-08 13:29:38 +0800

    Update email

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='aiml',
       version='1.0',
       description='ML Automation',
       author='MSDS ML',
-      author_email='edavid@aim.edu',
+      author_email='edjdavid@users.noreply.github.com',
       packages=find_packages(),
       install_requires=[
           'numpy',
